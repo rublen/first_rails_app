79b7a61027a84a5d388842efcd82cb72487e7663
lena <rublen77@gmail.com>
2017-08-08 12:30:09 +0300

    plus adding an image

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	has_many :taggings, dependent: :destroy
 	has_many :tags, through: :taggings
+	# has_many :attachments, dependent: :destroy
+	has_attached_file :image, dependent: :destroy
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 	def tag_list
 		tags.join(", ")
